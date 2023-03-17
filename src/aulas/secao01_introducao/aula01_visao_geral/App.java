da6cb7bc2552d9ae94882a7558e4e40eeffb5377
Wilson <wilsonprogram@gmail.com>
2023-03-17 15:31:55 -0300

    secao 01 - aula 01 - visao geral

---
+++ src/aulas/secao01_introducao/aula01_visao_geral/App.java
@@ -0,0 +1,13 @@
+package aulas.secao01_introducao.aula01_visao_geral;
+
+public class App {
+
+	
+	/** 
+	 * 
+	 * Está seção se destina apenas a uma apresentação do conteudo do curso
+	 *  
+	 *  
+	 *  */
+	
+}
