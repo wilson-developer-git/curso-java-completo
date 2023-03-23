341319515d3d2579cf3b543a84421c6b41784316
Wilson <wilsonprogram@gmail.com>
2023-03-23 15:14:42 -0300

    Seção 02 Conceitos de programação Aula 05 O que é preciso para fazer um programa de computador

--- src/aulas/secao02_conceitos_programacao/aula04_algoritmo_automacao_programa_computador/App.java
+++ src/aulas/secao02_conceitos_programacao/aula04_algoritmo_automacao_programa_computador/App.java
@@ -6,11 +6,9 @@ public class App {
 	/**
 	 * 
 	 *  Seção 02 Conceitos de programação - 
-	 *  Aula 04
 	 *  
-	 *   Algoritmo
-	 *   Automação
-	 *   Programa de computador
+	 *  Aula 04 Algoritmo, Automação e Programa de computador
+
 	 * 
 	 * */
 	
---
+++ src/aulas/secao02_conceitos_programacao/aula05_o_que_e_preciso_para_fazer_um_programa_computador/App.java
@@ -0,0 +1,15 @@
+package aulas.secao02_conceitos_programacao.aula05_o_que_e_preciso_para_fazer_um_programa_computador;
+
+public class App {
+
+	
+	/**
+	 * 
+	 *  Seção 02 Conceitos de programação - 
+	 *  
+	 *  Aula 05 O que é preciso para fazer um programa de computador 
+
+	 * 
+	 * */
+	
+}
