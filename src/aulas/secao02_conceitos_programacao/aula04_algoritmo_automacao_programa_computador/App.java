c5d83ab1aad546d6f62c79a44e969f1ec48dbac4
Wilson <wilsonprogram@gmail.com>
2023-03-23 15:04:06 -0300

    Seção 02 Conceitos de programação Aula 04 Algoritmo, Automação e Programa de computador

---
+++ src/aulas/secao02_conceitos_programacao/aula04_algoritmo_automacao_programa_computador/App.java
@@ -0,0 +1,17 @@
+package aulas.secao02_conceitos_programacao.aula04_algoritmo_automacao_programa_computador;
+
+public class App {
+
+	
+	/**
+	 * 
+	 *  Seção 02 Conceitos de programação - 
+	 *  Aula 04
+	 *  
+	 *   Algoritmo
+	 *   Automação
+	 *   Programa de computador
+	 * 
+	 * */
+	
+}
