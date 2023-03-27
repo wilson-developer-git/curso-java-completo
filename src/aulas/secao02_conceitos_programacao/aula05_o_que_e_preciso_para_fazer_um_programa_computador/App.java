e4a4af15f964adbdf71004a4d5e53be2c67ccd4f
Wilson <wilsonprogram@gmail.com>
2023-03-27 16:35:19 -0300

    Seção 02 Conceitos de programação Aula 06 Linguagem de programçaõp Lexica e Sintatica

--- src/aulas/secao02_conceitos_programacao/aula05_o_que_e_preciso_para_fazer_um_programa_computador/App.java
+++ src/aulas/secao02_conceitos_programacao/aula05_o_que_e_preciso_para_fazer_um_programa_computador/App.java
@@ -7,7 +7,9 @@ public class App {
 	 * 
 	 *  Seção 02 Conceitos de programação - 
 	 *  
-	 *  Aula 05 O que é preciso para fazer um programa de computador 
+	 *  Aula 06 O Linguagem de programçaõp Lexica e Sintatica
+	 *  Lexica = ortografia  = palavras isoladas
+	 *  Sintatica = gramatica = sentença como um todo
 
 	 * 
 	 * */
---
+++ src/aulas/secao02_conceitos_programacao/aula06_linguagem_de_programacao_lexica_sintatica/App.java
@@ -0,0 +1,15 @@
+package aulas.secao02_conceitos_programacao.aula06_linguagem_de_programacao_lexica_sintatica;
+
+public class App {
+
+	
+	/**
+	 * 
+	 *  Seção 02 Conceitos de programação - 
+	 *  
+	 *  Aula 05 O que é preciso para fazer um programa de computador 
+
+	 * 
+	 * */
+	
+}
