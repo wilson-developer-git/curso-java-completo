5d4c8af882681bcc1941d10c980a717bef4c997a
Wilson <wilsonprogram@gmail.com>
2023-03-27 17:01:38 -0300

    Seção 02 Conceitos de programação Aula 07 IDE ambiente integrado de desenvolvimento

--- src/aulas/secao02_conceitos_programacao/aula07_ide_ambiente_integrado_desenvolvimento/App.java
+++ src/aulas/secao02_conceitos_programacao/aula07_ide_ambiente_integrado_desenvolvimento/App.java
@@ -8,6 +8,9 @@ public class App {
 	 *  Seção 02 Conceitos de programação - 
 	 *  
 	 *  Aula 07 IDE anbiente integrado de desenvolvimento
+	 *  
+	 *  Esse commit foi esquecido entao fiz essa linha so pra atualizar.
+	 *  
 	 * */
 	
 }
