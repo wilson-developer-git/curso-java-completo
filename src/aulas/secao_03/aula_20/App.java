724cf5ba4dc71af2bec4acd4741630cda0ffd1a5
Wilson <wilsonprogram@gmail.com>
2023-05-27 10:28:03 -0300

    Aula 20 - Expressões Aritméticas

---
+++ src/aulas/secao_03/aula_20/App.java
@@ -0,0 +1,33 @@
+package aulas.secao_03.aula_20;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int x = 6; 
+		int y = 3, 
+		result = 0;
+
+		//adição
+		result = x + y;
+		System.out.println("6 + 3 = "+ result);
+		
+		//subtração
+		result = x - y;
+		System.out.println("6 - 3 = "+ result);
+		
+		//multiplicação
+		result = x * y;
+		System.out.println("6 * 3 = "+ result);
+		
+		//divisão
+		result = x / y;
+		System.out.println("6 / 3 = "+ result);
+		
+		//mod
+		result = x % y;
+		System.out.println("6 % 3 = "+ result);
+		
+	}
+	
+}
