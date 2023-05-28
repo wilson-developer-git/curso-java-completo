99cdc5abee804f37b9e29f61653bc63eaafaa3d0
Wilson <wilsonprogram@gmail.com>
2023-05-28 11:19:51 -0300

    Aula 26 - Entrada de dados em java - parte 2

---
+++ src/aulas/secao_04/aula_26/App.java
@@ -0,0 +1,34 @@
+package aulas.secao_04.aula_26;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int x;
+		String s1, s2, s3;
+		
+		x = sc.nextInt();
+		
+		sc.nextLine();//CONSUMINDO A QUEBRA DE LINHA PENDENTE
+		
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
+		
+		System.out.println("DADOS DIGITADOS:");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
