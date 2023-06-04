4480eec5788d0916c26d6f29d6c090ea27336f00
Wilson <wilsonprogram@gmail.com>
2023-06-04 15:54:00 -0300

    Aula 68 - Finalizando o programa

--- src/aulas/secao_08/aula_66/App.java
+++ src/aulas/secao_08/aula_66/App.java
@@ -21,7 +21,25 @@ public class App {
 		System.out.print("Quantity: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name +", "+ product.price +", "+ product.quantity);
+		System.out.println();
+		System.out.println("Product data: "+ product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: "+ product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product data: "+ product);
+		
 		
 		sc.close();
 		
