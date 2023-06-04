8129510b7ee8682895493a9bf84d91a205b6ce44
Wilson <wilsonprogram@gmail.com>
2023-06-04 15:27:46 -0300

    Aula 66 - Começando a resolver um segundo problema exemplo

---
+++ src/aulas/secao_08/aula_66/App.java
@@ -0,0 +1,31 @@
+package aulas.secao_08.aula_66;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import aulas.secao_08.aula_66.entities.Product;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product.name +", "+ product.price +", "+ product.quantity);
+		
+		sc.close();
+		
+		
+	}
+
+}
