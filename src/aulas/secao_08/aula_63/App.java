2b3e4b8d204220dff431bf26f866901c64b22e68
Wilson <wilsonprogram@gmail.com>
2023-06-04 10:31:47 -0300

    Aula 64 - Criando uma classe com três atributos para representar melhor o triângulo

--- src/aulas/secao_08/aula_63/App.java
+++ src/aulas/secao_08/aula_63/App.java
@@ -9,39 +9,35 @@ public class App {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double xA, xB, xC;
+		double xA, xB, xC , yA, yB, yC;
 		System.out.println("Enter the measures of tringle X: ");
 		xA = sc.nextDouble();
 		xB = sc.nextDouble();
 		xC = sc.nextDouble();
 		
+		System.out.println("Enter the measures of tringle Y: "); 
+		yA = sc.nextDouble(); 
+		yB = sc.nextDouble(); 
+		yC = sc.nextDouble();
+		
 		double p = (xA + xB + xC) / 2.0;
 		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		
+		p = (yA + yB + yC) / 2.0; 
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) *(p - yC));
+		
 		System.out.printf("Triangle X area %.4f%n", areaX);
+		System.out.printf("Triangle y area %.4f%n", areaY);
 	
+		if(areaX > areaY) 
+		{ 
+			System.out.println("Larger area: X"); 
+		}
+		else 
+		{
+			System.out.println("Larger area: Y"); 
+		}
+		
 		sc.close();
 	}
 }
-
-/* , yA, yB, yC */
-
-/*
- * System.out.println("Enter the measures of tringle Y: "); yA =
- * sc.nextDouble(); yB = sc.nextDouble(); yC = sc.nextDouble();
- */
-
-/*
- * p = (yA + yB + yC) / 2.0; double areaY = Math.sqrt(p * (p - yA) * (p - yB) *
- * (p - yC));
- */
-
-/*
- * System.out.printf("Triangle y area %.4f%n", areaY);
- * 
- * if(areaX > areaY) { System.out.println("Larger area: X"); } else {
- * System.out.println("Larger area: Y"); }
- */
-
-
-
---
+++ src/aulas/secao_08/aula_64/entities/Triangle.java
@@ -0,0 +1,9 @@
+package aulas.secao_08.aula_64.entities;
+
+public class Triangle {
+
+	public double a;
+	public double b;
+	public double c;
+	
+}
