c0877577ce1194081baa585a3a7ad120b1f9e8bc
Wilson <wilsonprogram@gmail.com>
2023-06-05 06:41:10 -0300

    Aula 78 - Encapsulamento

---
+++ src/aulas/secao_09/aula_78/App.java
@@ -0,0 +1,46 @@
+package aulas.secao_09.aula_78;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import aulas.secao_09.aula_78.entities.Product;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		
+		Product product = new Product(name, price);
+		
+		System.out.println();
+		System.out.println("Product data: "+ product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: "+ product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Product data: "+ product);
+		
+		sc.close();
+		
+	}
+
+}
