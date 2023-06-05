f79874fd8aedfd4d54afeb7191f987f94e638bfe
Wilson <wilsonprogram@gmail.com>
2023-06-05 20:52:51 -0300

    Aula 110 - Instanciando data-hora java

---
+++ src/aulas/secao_11/aula_110/App.java
@@ -0,0 +1,36 @@
+package aulas.secao_11.aula_110;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		LocalDate d01 = LocalDate.now();
+		LocalDateTime d02 = LocalDateTime.now();
+		Instant d03 = Instant.now();
+		
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+		Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
+		
+		LocalDate d08 = LocalDate.parse("20/06/2023", df);
+		
+		System.out.println("d01 = "+ d01);
+		System.out.println("d02 = "+ d02);
+		System.out.println("d03 = "+ d03);
+		System.out.println("d04 = "+ d04);
+		System.out.println("d05 = "+ d05);
+		System.out.println("d06 = "+ d06);
+		System.out.println("d07 = "+ d07);
+		System.out.println("d08 = "+ d08);
+		
+	}
+
+}
