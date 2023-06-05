cdfa1cbc18924941398b645322fd1b1370c5c600
Wilson <wilsonprogram@gmail.com>
2023-06-05 11:26:30 -0300

    Aula 96 -Laço for each

---
+++ src/aulas/secao_10/aula_96/App.java
@@ -0,0 +1,15 @@
+package aulas.secao_10.aula_96;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		String[] vect = new String[] {"João", "Maria", "Jose"};
+		
+		for (String nome : vect) {
+			System.out.println(nome);
+		}
+		
+	}
+
+}
