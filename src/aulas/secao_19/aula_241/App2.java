8f8552a33631345b1a7d51b2f3643cd067cf51ea
Wilson <wilsonprogram@gmail.com>
2023-06-16 10:49:24 -0300

    Aula 241 - Curingas delimitados

---
+++ src/aulas/secao_19/aula_241/App2.java
@@ -0,0 +1,27 @@
+package aulas.secao_19.aula_241;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> intList = new ArrayList<Integer>();
+		intList.add(10);
+		intList.add(5);
+		List<? extends Number> list = intList;
+		Number x = list.get(0);
+		//list.add(20); // erro de compilacao no add
+		
+		List<Object> myObjs = new ArrayList<Object>();
+		myObjs.add("Maria");
+		myObjs.add("Alex");
+		List<? super Number> myNums = myObjs;
+		myNums.add(10);
+		myNums.add(3.14);
+		//Number x = myNums.get(0); // erro de compilacao no get
+		
+	}
+
+}
---
+++ src/aulas/secao_19/aula_241/entities/Shape.java
@@ -0,0 +1,6 @@
+package aulas.secao_19.aula_241.entities;
+
+public interface Shape {
+
+	double area();
+}
